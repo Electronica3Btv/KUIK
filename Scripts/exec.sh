b55b64f2a76e3d1c696a738a2a9fd56276f0c28c
Electronica3Btv <145098949+Electronica3Btv@users.noreply.github.com>
2024-06-06 14:15:22 +0200

    Update exec.sh

--- Scripts/exec.sh
+++ Scripts/exec.sh
@@ -1,1 +1,4 @@
-chmod +x /home/intelio/KUIK_Scripts/Scripts/*.sh 
+chmod +x /home/intelio/KUIK_Scripts/Scripts/*.sh
+chmod +x /home/intelio/KUIK_Scripts/Scripts/ParcelIntelio/*.sh
+chmod +x /home/intelio/KUIK_Scripts/Scripts/Comunes/*.sh
+
