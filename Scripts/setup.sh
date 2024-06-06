fa844f2facb9c10d330bc468f9b674ba96961de5
Electronica3Btv <145098949+Electronica3Btv@users.noreply.github.com>
2024-06-06 14:29:32 +0200

    Update setup.sh

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -1,6 +1,6 @@
 #la imagen tendra que tener un script que convierte en ejecutables los sh de todos los repositorios
 
-sudo dpkg -i /anydesk.
+sudo dpkg -i /home/intelio/Downloads/anydesk_6.2.1-1_amd64.deb
 echo 1Kuik21Kuik2 | anydesk --set-password
 echo the anydesk ID is:
 anydesk --get-id
