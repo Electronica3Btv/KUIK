8d85df7e3cc76cd5d5d82609d4fabe6a2d664ae0
Electronica3Btv <145098949+Electronica3Btv@users.noreply.github.com>
2024-06-12 10:04:43 +0200

    Update setup.sh

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -1,4 +1,8 @@
 #la imagen tendra que tener un script que convierte en ejecutables los sh de todos los repositorios
+chmod +x /home/intelio/KUIK_Scripts/Scripts/*.sh
+chmod +x /home/intelio/KUIK_Scripts/Scripts/ParcelIntelio/*.sh
+chmod +x /home/intelio/KUIK_Scripts/Scripts/Comunes/*.sh
+
 git config credential.helper store
 
 sudo dpkg -i /home/intelio/Downloads/anydesk_6.2.1-1_amd64.deb
